08b54734ae55815be4299c6b751624042b939941
Van Luu <thanhvanluu.3199@gmail.com>
2025-05-30 01:14:42 +0700

    init: add necessary libs

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'ui';
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.loadIcons();
+  }
+
+  loadIcons() {
+    const iconLabels = ["crm"];
+    for (let icon of iconLabels) {
+      const path = "../assets/images/icons/icon-";
+      this.matIconRegistry.addSvgIcon(
+        icon,
+        this.domSanitizer.bypassSecurityTrustResourceUrl(`${path}${icon}.svg`)
+      );
+    }
+  }
 }
